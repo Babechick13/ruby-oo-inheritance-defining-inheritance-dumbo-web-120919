bfcc27cb7d160f2439ac748393263fcabbf5bc05
Jessica Natovich <jessicanatovich@yahoo.com>
2019-12-13 14:16:25 +0000

    Automatically backed up by Learn

--- lib/car.rb
+++ lib/car.rb
@@ -1,5 +1,13 @@
-class Car < Vehicle
-  def go 
-"VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+describe "#go" do
+    it "returns 'VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!'" do 
+    it "returns 'vrrrrrrrooom!'" do 
+      expect(car.go).to eq("VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!")
+    end 
+  end
+@@ -34,4 +34,4 @@
+      expect(car.fill_up_tank).to eq("filling up!")
+    end
+  end
 end
 end 
+0 comments on commit 0d36865
